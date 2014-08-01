ef258621f5807937f34b063bc08a70960f6978b5
Balazs Ree <ree@greenfinity.hu>
2014-08-01 14:25:26 +0200

    Implement wordlist based on current language, also improve error handling

--- plone_hyphenator/static/plone_hyphenator.js
+++ plone_hyphenator/static/plone_hyphenator.js
@@ -1,11 +1,41 @@
 +function($) {
 
+// simple log functions that only log one parameter.
+function info(txt) {
+  var c = window.console;
+  if (c && c.log) {
+    c.log(txt);
+  }
+}
+
+function error(txt) {
+  var c = window.console;
+  if (c && c.error) {
+    c.error(txt);
+  }
+}
+
 // get a query param from the request
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// split a file name to base + ext parts
+function splitName(name) {
+  var dotPos = name.lastIndexOf('.'),
+    base = name,
+    ext = '';
+  if (dotPos != -1) {
+    base = name.substring(0, dotPos);
+    ext = name.substring(dotPos);
+  }
+  return {
+    base: base,
+    ext: ext
+  };
 }
 
 var controller = {
@@ -15,6 +45,24 @@ var controller = {
     this.options = options;
     this.config();
     Hyphenator.run();
+  },
+  detectLanguage: function() {
+    // Dry run, just to detect languages.
+    // Normally, one would call the corresponding function, but Hyphenator
+    // hides these in a closure. So we run the whole shebang but with a
+    // zero selection list to make it snappy.
+    Hyphenator.config({
+      selectorfunction: function () {
+        return [];
+      }
+    });
+    // After the hyphenator has detected the current language,
+    // it sets it as 'lang' attribute on the html. We will
+    // need to use this to fetch our exception list for that language.
+    var lang = $('html')[0].lang;
+    // info('LANG: ' + lang);
+    this.lang = lang;
+    return lang;
   },
   config: function() {
     var options = this.options;
@@ -38,9 +86,8 @@ var controller = {
         exceptions.push(val.toLowerCase());
         exceptions.push(val.charAt(0).toUpperCase() + val.slice(1).toLowerCase());
       }
-      // XXX German language only
-      // (wordlist will be ignored to all other languages)
-      Hyphenator.addExceptions('de', exceptions.join(', '));
+      // add wordlist for current language
+      Hyphenator.addExceptions(this.lang, exceptions.join(', '));
     }
   }
 };
@@ -48,21 +95,31 @@ var controller = {
 
 $(function() {
   var hyphenatorSelector = $('meta[name="plone-hyphenator-selector"]').attr('content');
-  var wordlistUrl = $('meta[name="plone-hyphenator-wordlist-url"]').attr('content');
-  if (wordlistUrl) {
+  var wordlistBaseUrl = $('meta[name="plone-hyphenator-wordlist-url"]').attr('content');
+  if (wordlistBaseUrl) {
+    // detect language. If xxxx.json was given, the url will be xxxxx_en.json, xxxx_de.json.
+    var lang = controller.detectLanguage();
+    var split = splitName(wordlistBaseUrl);
+    var wordlistUrl = split.base + '_' + lang.toLowerCase() + split.ext;
     $.ajax({
       url: wordlistUrl
-    }).then(function(data) {
+    }).done(function(data) {
       var wordlist;
       try {
         wordlist = JSON.parse(data);
       } catch (exc) {
-        console.log('JSON parse error, wordlist ignored -' + wordlist);
+        error('JSON parse error, wordlist ignored - ' + wordlist);
         wordlist = '';
       }
       controller.init({
         hyphenatorSelector: hyphenatorSelector,
         wordlist: wordlist
+      });
+    }).fail(function(jqXHR, textStatus) {
+      error('Wordlist file could not be read, wordlist ignored [' + textStatus + ']');
+      // no wordlist
+      controller.init({
+        hyphenatorSelector: hyphenatorSelector,
       });
     });
   } else {
