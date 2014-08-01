f07ee0f19b930666b3ef0f66bfdbdfb2b449681e
Balazs Ree <ree@greenfinity.hu>
2014-08-01 16:49:52 +0200

    Progress on management UI, overlay

--- plone_hyphenator/static/plone_hyphenator_manage.js
+++ plone_hyphenator/static/plone_hyphenator_manage.js
@@ -6,25 +6,42 @@ var data = $('html').data(),
 // controller for management ui
 var manageController = {
   init: function(options) {
+    var self = this;
     this.options = options;
+    var el = this.el = $('#plone-hyphenator-overlay');
+    el.overlay({
+    });
+    el.find('.close').click(function(evt) {
+      self.close();
+      return false;
+    });
+    el.find('.submit').click(function(evt) {
+      self.save();
+      return false;
+    });
   },
   open: function() {
     var lang = module.detectLanguage();
     console.log('OPEN', lang);
+    this.el.overlay().load();
   },
   close: function() {
     console.log('CLOSE');
+    this.el.overlay().close();
+  },
+  save: function() {
+    console.log('SAVE');
+    this.close();
   }
 };
+
 
 // export module
 module.manageController = manageController;
 
 $(function() {
-
   manageController.init({
   });
-
 });
 
 }(jQuery);
