a3b00ff98f21eccb50b3f024d0a02ace7ebcde22
alan runyan <runyaga@gmail.com>
2014-11-17 22:07:27 -0600

    reduce flicker

--- plone_hyphenator/static/plone_hyphenator.js
+++ plone_hyphenator/static/plone_hyphenator.js
@@ -117,6 +117,7 @@
           // https://code.google.com/p/hyphenator/wiki/en_PublicAPI#property_intermediatestate
           intermediatestate : 'visible',
           minwordlength : options.minwordlength,
+          intermediatestate : 'visible',
           // Specify a fallback language in any case
           defaultlanguage: options.lang,
           selectorfunction: function () {
