298f02a4a5f64f6cb6428fc191d1bda63f02ba82
Franco Pellegrini <frapell@gmail.com>
2015-01-05 19:39:10 +0000

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.3'
+version = '0.4.dev0'
 
 setup(name='plone_hyphenator',
     version=version,
